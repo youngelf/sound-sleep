a6ee7cb4f8d80a343e11c4671cfcb49c7e062c12
Vikram Aggarwal <vikram@eggwall.com>
2013-02-12 22:56:48 -0800

    Random movement of the clock works. Views are only measured after they are laid out the first time around, so initially their {measured,}{width,height} are all zero. Interesting!

--- src/com/eggwall/BabyMusic/BabyActivity.java
+++ src/com/eggwall/BabyMusic/BabyActivity.java
@@ -1,9 +1,9 @@
 package com.eggwall.BabyMusic;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 public class BabyActivity extends Activity {
@@ -13,13 +13,53 @@ public class BabyActivity extends Activity {
     private static final int WHITE_NOISE = 2;
     /** Set to MUSIC or WHITE_NOISE */
     private int mTypePlaying = 0;
+    private final Handler mHandler = new Handler();
+    private int mWidth;
+    private int mHeight;
+    // Change clock location every minute
+    private static final int DELAY = 1 * 60 * 1000;
+
+    private final Runnable mChangeClockLocation = new Runnable() {
+        @Override
+        public void run() {
+            changeClockLocation();
+            postClockChange();
+        }
+    };
+
+    /**
+     * Moves the clock to some random location.
+     */
+    private final void changeClockLocation() {
+        final View v = findViewById(R.id.clock);
+        if (mWidth == 0) {
+            final View topLevel = findViewById(R.id.toplevel);
+            mWidth = topLevel.getRight();
+        }
+        if (mHeight == 0) {
+            final View topLevel = findViewById(R.id.toplevel);
+            mHeight = topLevel.getBottom();
+        }
+        final int x = (int)(Math.random()*(mWidth - v.getWidth()));
+        v.setX(x);
+        final int y = (int)(Math.random()*(mHeight- v.getHeight()));
+        v.setY(y);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        final ActionBar actionBar = getActionBar();
-        actionBar.hide();
+        // Go full screen.
+        (getActionBar()).hide();
+        // Hide the System status bar
+        final View topLevel = findViewById(R.id.toplevel);
+        topLevel.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        postClockChange();
+    }
+
+    private final void postClockChange() {
+        mHandler.postDelayed(mChangeClockLocation, DELAY);
     }
 
     /**
