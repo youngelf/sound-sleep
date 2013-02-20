013673aa4bf8a361e67933d7a15f1864dffb2124
Vikram Aggarwal <vikram@eggwall.com>
2013-02-19 20:51:17 -0800

    Use a Random object and reduce instance variables a bit

--- src/com/eggwall/BabyMusic/AudioService.java
+++ src/com/eggwall/BabyMusic/AudioService.java
@@ -30,6 +30,7 @@ import android.util.Log;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Runs the music in the background and holds a wake lock during the duration of music playing.
@@ -37,11 +38,8 @@ import java.util.Arrays;
 public class AudioService extends Service implements MediaPlayer.OnErrorListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
     /** For logging */
     private static final String TAG = "AudioService";
-    /**
-     * The tag used to pass the music type. Can only be MUSIC or WHITE_NOISE
-     */
+    /** The tag used to pass the music type. Can only be MUSIC or WHITE_NOISE */
     public static final String TYPE = "type";
-    private MediaPlayer mPlayer;
     /** Stop playing any audio. */
     public static final int SILENCE = 0;
     /** Play music from the SD card */
@@ -49,17 +47,22 @@ public class AudioService extends Service implements MediaPlayer.OnErrorListener
     /** Play standard white noise file (included in the application */
     public static final int WHITE_NOISE = 2;
 
-    /** Set to MUSIC or WHITE_NOISE */
-    private int mTypePlaying = 0;
+    /** This represents in invalid position in the list and also an invalid resource. */
+    private static final int INVALID_POSITION = -1;
     /** Name of the directory in the main folder containing baby music */
     private final static String BABY_MUSIC_DIR = "babysong";
+
+    /** Single instance of random number generator */
+    private final Random random = new Random();
+
+    /** The object that actually plays the music on our behalf. */
+    private MediaPlayer mPlayer;
+    /** Set to MUSIC or WHITE_NOISE */
+    private int mTypePlaying = 0;
     /** The actual directory that corresponds to the external SD card. */
     private File mBabyDir;
     /** Names of all the songs */
     private String[] mFilenames;
-    /** Which location are we currently playing, -1 if playing none */
-    private static final int INVALID_POSITION = -1;
-    private int mPosition = INVALID_POSITION;
 
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
@@ -102,13 +105,15 @@ public class AudioService extends Service implements MediaPlayer.OnErrorListener
         mPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
         mPlayer.setOnPreparedListener(this);
         final int resourceToPlay;
+        final int nextPosition;
         if (type == WHITE_NOISE) {
             Log.v(TAG, "Playing browninan noise");
             resourceToPlay = R.raw.noise;
+            nextPosition = INVALID_POSITION;
         } else {
             // Try to open the SD card and read from there. If nothing is found, play the
             // default music.
-            final int nextPosition = nextTrackFromCard();
+            nextPosition = nextTrackFromCard();
             if (nextPosition == -1) {
                 Log.v(TAG, "No SD card music found, playing default music");
                 resourceToPlay = R.raw.all_of_me;
@@ -119,7 +124,7 @@ public class AudioService extends Service implements MediaPlayer.OnErrorListener
         try {
             if (resourceToPlay == INVALID_POSITION) {
                 // Play files, not resources. Play the music file given here.
-                final String file = mBabyDir.getAbsolutePath() + File.separator + mFilenames[mPosition];
+                final String file = mBabyDir.getAbsolutePath() + File.separator + mFilenames[nextPosition];
                 Log.d(TAG, "Now playing " + file);
                 mPlayer.setDataSource(file);
                 mPlayer.setOnCompletionListener(this);
@@ -158,13 +163,7 @@ public class AudioService extends Service implements MediaPlayer.OnErrorListener
                 return INVALID_POSITION;
             }
         }
-        final int size = mFilenames.length;
-        // A random position from the list.
-        mPosition = (int)(Math.random() * (size + 1));
-        if (mPosition < 0 || mPosition >= size) {
-            mPosition = 0;
-        }
-        return mPosition;
+        return random.nextInt(mFilenames.length + 1);
     }
 
     /**
